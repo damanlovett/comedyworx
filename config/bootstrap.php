256a42997776851f9f03c63993d9f3b8328fa4f5
Eddie Lovett <eddie@lovettcreations.org>
2018-04-08 22:12:21 -0400

    inital Push

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -207,6 +207,13 @@ Type::build('timestamp')
  *
  */
 
+
+// load user management plugin
+    Plugin::load('Usermgmt', ['autoload' => true, 'bootstrap' => true, 'routes' => true]);
+
+
+
+
 /*
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
