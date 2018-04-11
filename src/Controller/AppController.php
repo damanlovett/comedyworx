46304b0798e38ca197bd2fe4993698a12ff0f607
Eddie Lovett <eddie@lovettcreations.org>
2018-04-10 23:07:33 -0400

    initial commit to reinstate files

---
+++ src/Controller/AppController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+use Cake\Event\Event;
+
+class AppController extends Controller {
+
+    public $components = ['Flash', 'Auth', 'Usermgmt.UserAuth'/*, 'Security', 'Csrf'*/];
+    public $helpers = ['Usermgmt.UserAuth', 'Usermgmt.Image', 'Form'];
+
+    /* Override functions */
+    public function paginate($object = null, array $settings = []) {
+        $sessionKey = sprintf('UserAuth.Search.%s.%s', $this->request['controller'], $this->request['action']);
+        if($this->request->session()->check($sessionKey)) {
+            $persistedData = $this->request->session()->read($sessionKey);
+            if(!empty($persistedData['page_limit'])) {
+                $this->paginate['limit'] = $persistedData['page_limit'];
+            }
+        }
+        return parent::paginate($object, $settings);
+    }
+
+    public function beforeRender(Event $event) {
+        if(!array_key_exists('_serialize', $this->viewVars) && in_array($this->response->type(), ['application/json', 'application/xml'])) {
+            $this->set('_serialize', true);
+        }
+    }
+}
